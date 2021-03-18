b831c30d37ea7afc1252f5286ec44d4100411090
nityo <nityo.git@nityo.com>
2021-03-19 02:18:19 +0530

    Caching

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,28 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: {
-    index: {
-      import: "./src/index.js",
-    },
-  },
+  entry: "./src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Development",
+      title: "Caching",
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  optimization: {
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
   },
 };
